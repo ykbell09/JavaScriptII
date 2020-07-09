b031531b418ae3d40af0b6ce2b7418647a9e57e8
Yvette Bell <ykbell09@gmail.com>
2020-07-08 18:19:19 -0700

    week 1 assignment completion

---
+++ assignments/week1/main.js
@@ -0,0 +1,9 @@
+const showMedia = () => {
+    document.querySelector('#apolloVid').style.display = 'inline';
+    document.querySelector('#apolloPod').style.display = 'inline';
+}
+
+const hideMedia = () => {
+    document.querySelector('#apolloVid').style.display = 'none';
+    document.querySelector('#apolloPod').style.display = 'none';
+};
