0e81dc32b02f27192104c295e6b9659b2b633295
Yvette Bell <ykbell09@gmail.com>
2020-08-04 13:13:38 -0700

    week 5 initial commit, updates to final project files

---
+++ assignments/week5/js/cart.js
@@ -0,0 +1,22 @@
+// Week 5 Assignment - Shopping Cart
+
+// You can use this data for seeding your cart
+// Or you can create your own
+
+books = [
+    {
+        title: 'Absolute Java',
+        qty: 1,
+        price: 114.95
+    },
+    {
+        title: 'Pro HTML5',
+        qty: 1,
+        price: 27.95
+    },
+    {
+        title: 'Head First HTML5',
+        qty: 1,
+        price: 27.89
+    }
+];
